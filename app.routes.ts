e149785ae8139c055a00c017bbb7f99c1ded5ea0
ST-DevNinja <151509323+ST-DevNinja@users.noreply.github.com>
2023-11-20 18:31:57 +0500

    src files
    Please do check what is the nearest possible issue for my router link not working...

---
+++ app.routes.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './components/products/products.component';
+
+export const routes: Routes = [
+{
+    path: '',
+    component: ProductsComponent
+},
+{
+    path: 'products',
+    component: ProductsComponent
+}
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  
+})
+
+export class AppRoutingModule {}
+
